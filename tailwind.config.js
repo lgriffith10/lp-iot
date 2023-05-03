f1337bd9054f40a09e26e8448cf2ba048ca3c259
lgriffithdev <lgriffith.dev@gmail.com>
2023-05-03 17:08:19 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "src/**/*.{scss,js,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
